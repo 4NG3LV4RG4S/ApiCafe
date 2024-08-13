1ec66d92204f82311518a15c95b3ec180a91dfa2
4NG3LV4RG4S <juan_vargas@autorefax.com>
2024-08-12 21:33:12 -0600

    first commit

---
+++ Ixhuatlan.E-Coffe.Backend.Controllers/Authorization/AuthorizationController.cs
@@ -0,0 +1,28 @@
+﻿using Ixhuatlan.E_Coffe.Backend.Entities.DTOs.Authorization;
+using Ixhuatlan.E_Coffe.Backend.Ports.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ixhuatlan.E_Coffe.Backend.Controllers.Authorization;
+
+[ApiController]
+[Route("api/Token/[controller]")]
+public class AuthorizationController : ControllerBase
+{
+    private readonly ITokenInputPort InputPort;
+
+    public AuthorizationController(ITokenInputPort inputPort)
+    {
+        InputPort = inputPort;
+    }
+
+    [HttpPost("GetTokenAuth")]
+    public IActionResult GetToken([FromBody] UserDto data)
+    {
+        if (InputPort.ValidateCredentials(data.UserName, data.Password))
+        {
+            TokenRequestDto token = InputPort.GenerateToken(data.UserName);
+            return Ok(token);
+        }
+        return Unauthorized();
+    }
+}
---
+++ Ixhuatlan.E-Coffe.Backend.Entities/DTOs/Authorization/UserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Ixhuatlan.E_Coffe.Backend.Entities.DTOs.Authorization;
+
+public class UserDto
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
